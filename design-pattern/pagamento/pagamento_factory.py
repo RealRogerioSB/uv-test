a9b9d3ca556f3297115f5ccce7aeeb868c64d9c0
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-07-13 17:50:03 -0300

    added commit...

---
+++ design-pattern/pagamento/pagamento_factory.py
@@ -0,0 +1,13 @@
+from pagamento_pix import PagamentoPIX
+from pagamento_cartao import PagamentoCartao
+
+
+class PagamentoFactory:
+    @staticmethod
+    def criar_pagamento(tipo):
+        if tipo == "pix":
+            return PagamentoPIX()
+        elif tipo == "cartao":
+            return PagamentoCartao()
+        else:
+            raise ValueError(f"Tipo de pagamento '{tipo}' não suportado")

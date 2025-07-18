f745899928eb5a2f9976906e7148badec80c7685
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-07-17 22:18:47 -0300

    updated commit...

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-import os
-import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import (SMTP, SMTPAuthenticationError, SMTPConnectError,
+                     SMTPException, SMTPServerDisconnected)
 
-while True:
-    os.system("cls" if os.name == "nt" else "clear")
 
-    nome = input("Digite seu nome (para encerrar basta dar ENTER: ")
+def send_email(subject, body: str, sender: str, password: str, recipients: list[str]) -> None:
+    msg: MIMEMultipart = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = ", ".join(recipients)
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
 
-    if nome:
-        print(f"Olá, {nome}! Seja bem-vindo(a)!")
-        time.sleep(1)
-    else:
-        print("Que pena... Tchau!")
-        break
+    with SMTP("smtp.gmail.com", 587) as smtp_server:
+        smtp_server.starttls()
+        smtp_server.login(sender, password)
+        try:
+            smtp_server.sendmail(sender, recipients, msg.as_string())
+        except SMTPAuthenticationError:
+            print("Erro de autenticação com o servidor SMTP.")
+        except (SMTPConnectError, SMTPServerDisconnected):
+            print("Erro/falha de conexão com o servidor SMTP.")
+        except SMTPException as e:
+            print(f"Erro de exceção desconhecida: {e}")
+        else:
+            print("E-mail enviado con sucesso!")
+
+
+send_email(
+    sender="rogerioballoussier@gmail.com",
+    password="tzdk mqxa ltxe qrha",
+    subject="Teste de envio de E-mail pelo Python",
+    body="Esse é o corpo de mensagem de texto.",
+    recipients=["rogerioballoussier@gmail.com", "rogerioballoussier@bb.com.br"],
+)
